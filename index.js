268ead81c675d8a8264414e640bb0b8d7e6f9a9e
Melyn Atieno Were <melynatieno@gmail.com>
2022-06-14 07:48:20 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ function moveDodgerLeft() {
     }
   }
 
+  document.addEventListener("keydown", function(e){
+    if (e.key === "ArrowLeft"){
+        moveDodgerLeft()
+    }
+})
+
 function moveDodgerRight(){
     const Rightside = dodger.style.left.replace("px", "")
     const left = parseInt(Rightside, 10)
@@ -16,3 +22,9 @@ function moveDodgerRight(){
         dodger.style.left = `${left+1}px`
     }
 }
+
+document.addEventListener("keydown", function(e){
+    if (e.key === "ArrowRight"){
+        moveDodgerRight()
+    }
+})
