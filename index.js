9e2e15d61e273b4b186d3c06c9e53be4544087e4
Melyn Atieno Were <melynatieno@gmail.com>
2022-06-14 07:42:48 +0300

    commit tree 8e5d58c3379e93480426d51262bfe869f59ce8b1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+function moveDodgerRight(){
+    const Rightside = dodger.style.left.replace("px", "")
+    const left = parseInt(Rightside, 10)
+
+    if (left>0){
+        dodger.style.left = `${left+1}px`
+    }
+}
